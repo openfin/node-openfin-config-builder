71f814345c2ce93718987b6899063366557ec926
Ricardo de pena <ricardo.depena@gmail.com>
2014-12-29 15:29:40 -0500

    added jsbeautifier

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var configBuilder = require('./lib/config-builder.js'),
     configUpdater = require('./lib/config-updater.js');
 
 module.exports = {
-    create : configBuilder.create,
-    update : configUpdater.update
+    create: configBuilder.create,
+    update: configUpdater.update
 };
