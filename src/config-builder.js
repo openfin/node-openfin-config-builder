13fc79f6abe2ada89f9a77caec7bcecd5519a926
Ricardo de pena <ricardo@isnull.co>
2018-11-28 15:32:47 -0500

    Fix security concearns and remove old dependencies.

--- src/config-builder.js
+++ src/config-builder.js
@@ -1,24 +1,23 @@
 'use strict';
 
 var configWritter = require('./config-writter'),
-    _ = require('lodash'),
     template = require('./config-template.json');
 
 function create(options, configPath, baseConfig) {
     var config = baseConfig || template;
 
     //extend config the options object
-    _.chain(options)
-        .keys()
-        .forEach(function(key) {
-            //if the config does not have the key or if its a value we simply copy it.
-            if (!config[key] || typeof options[key] !== 'object') {
-                config[key] = options[key];
-                //if its an object we extent it.
-            } else {
-                _.extend(config[key], options[key]);
-            }
-        });
+    Object.keys(options).forEach(function(key) {
+        //if the config does not have the key or if its a value we simply copy it.
+        if (!config[key] || typeof options[key] !== 'object') {
+            config[key] = options[key];
+            //if its an object we extent it.
+        } else {
+
+            //_.extend(config[key], options[key]);
+            config[key] = Object.assign(config[key], options[key]);
+        }
+    });
 
     //add a random uuid if none exists:
     if (config.startup_app && !config.startup_app.uuid) {
